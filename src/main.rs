8932cfe27a42776e646292e51b21039a1ea693c7
vividDuck <122105925+vividDuck@users.noreply.github.com>
2023-08-17 19:36:56 +0000

    working threads, test

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::thread;
-use std::env;
+
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
@@ -44,63 +44,39 @@ fn main() {
     // Get the args
     let args = Args::parse();
 
-    /// Read the config
+    // Read the config
     let f = std::fs::File::open(args.config).expect("Could not open file.");
-    let mut scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    let scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
     println!("{:?}", scrape_config);
 
     // let mut commands = Vec::new();
+    let mut children = vec![];
 
     for wisker in scrape_config.wiskers {
-        // let command = format!(
-        //     "{} {}",
-        //     wisker.binary, wisker.args
-        // );
-        // let _ = &commands.push(command.to_string());
-        // println!(
-        //     "name: {}, binary: {}, args {}",
-        //     wisker.name, wisker.binary, wisker.args
-        // );
-
-        let mut children = vec![];
-        
+        // Create thread per binary in config        
         let child = thread::spawn(move || {
             let output = Command::new(&wisker.binary)
                 .arg(&wisker.args)
                 .output()
                 .expect("Failed to execute command");
             
+            println!("[+] Ran {}", &wisker.name);
             output.stdout
         });
 
+        
         children.push(child);
     }
-//
-    // let mut children = vec![];
 
-    // for (program, args) in &commands {
-    //     let args: Vec<&str> = args.split_whitespace().collect();
-        
-    //     let child = thread::spawn(move || {
-    //         let output = Command::new(program)
-    //             .args(args)
-    //             .output()
-    //             .expect("Failed to execute command");
-            
-    //         output.stdout
-    //     });
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("output.txt")
+        .expect("Failed to open 'output.txt'");
 
-    //     children.push(child);
-    // }
-
-    // let mut file = OpenOptions::new()
-    //     .write(true)
-    //     .append(true)
-    //     .open("output.txt")
-    //     .expect("Failed to open 'output.txt'");
-
-    // for child in children {
-    //     let output = child.join().unwrap();
-    //     file.write_all(&output).expect("Failed to write to 'output.txt'");
-    // }
+    for child in children {
+        let output = child.join().unwrap();
+        file.write_all(&output).expect("Failed to write to 'output.txt'");
+    }
 }
