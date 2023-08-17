57cde2a6e94dfbe578fe0ac9e740dfecd6752ca3
vividDuck <122105925+vividDuck@users.noreply.github.com>
2023-08-17 15:41:58 +0000

    cli, yaml-config, parallel

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,72 @@
-use std::process::Command;
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+use std::process::{Command, Output};
+use std::time::Duration;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::thread;
 use std::env;
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    wiskers: Vec<Wiskers>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Wiskers {
+  name: String,
+  binary: String,
+  args: String,
+  outfolder: String,
+  #[serde(default)]
+  outfile: String,
+  #[serde(default)]
+  choco: String,
+  #[serde(default)]
+  github: String,
+  #[serde(default)]
+  deps_choco: String,
+  #[serde(default)]
+  deps_github: String,
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Config file
+    #[arg(short, long)]
+    config: String,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let commands: Vec<(&str, &str)> = args[1..]
-        .iter()
-        .map(|arg| {
-            let parts: Vec<&str> = arg.splitn(2, ' ').collect();
-            (parts[0], *parts.get(1).unwrap_or(&""))
-        })
-        .collect();
+    // Get the args
+    let args = Args::parse();
 
-    let mut children = vec![];
+    /// Read the config
+    let f = std::fs::File::open(args.config).expect("Could not open file.");
+    let mut scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    println!("{:?}", scrape_config);
 
-    for (program, args) in &commands {
-        let args: Vec<&str> = args.split_whitespace().collect();
+    // let mut commands = Vec::new();
+
+    for wisker in scrape_config.wiskers {
+        // let command = format!(
+        //     "{} {}",
+        //     wisker.binary, wisker.args
+        // );
+        // let _ = &commands.push(command.to_string());
+        // println!(
+        //     "name: {}, binary: {}, args {}",
+        //     wisker.name, wisker.binary, wisker.args
+        // );
+
+        let mut children = vec![];
         
         let child = thread::spawn(move || {
-            let output = Command::new(program)
-                .args(args)
+            let output = Command::new(&wisker.binary)
+                .arg(&wisker.args)
                 .output()
                 .expect("Failed to execute command");
             
@@ -30,15 +75,32 @@ fn main() {
 
         children.push(child);
     }
+//
+    // let mut children = vec![];
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("output.txt")
-        .expect("Failed to open 'output.txt'");
+    // for (program, args) in &commands {
+    //     let args: Vec<&str> = args.split_whitespace().collect();
+        
+    //     let child = thread::spawn(move || {
+    //         let output = Command::new(program)
+    //             .args(args)
+    //             .output()
+    //             .expect("Failed to execute command");
+            
+    //         output.stdout
+    //     });
 
-    for child in children {
-        let output = child.join().unwrap();
-        file.write_all(&output).expect("Failed to write to 'output.txt'");
-    }
+    //     children.push(child);
+    // }
+
+    // let mut file = OpenOptions::new()
+    //     .write(true)
+    //     .append(true)
+    //     .open("output.txt")
+    //     .expect("Failed to open 'output.txt'");
+
+    // for child in children {
+    //     let output = child.join().unwrap();
+    //     file.write_all(&output).expect("Failed to write to 'output.txt'");
+    // }
 }
--- src/main__.rs
+++ src/main__.rs
@@ -1,45 +1,5 @@
-use clap::Parser;
-use serde::{Deserialize, Serialize};
-use serde_yaml::{self};
 
-use std::process::{Command, Output};
-use std::thread;
-use std::time::Duration;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Config {
-    wiskers: Vec<Wiskers>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Wiskers {
-  name: String,
-  binary: String,
-  args: String,
-  outfolder: String,
-  outfile: String,
-  choco: String,
-  github: String,
-  deps_choco: String,
-  deps_github: String,
-}
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(long, default_value_t = 1)]
-    count: u8,
-
-    /// Config file
-    #[arg(short, long)]
-    config: String,
-}
 
 fn run_cmds() {
     let binaries = vec!["calc.exe", "notepad.exe"];
@@ -57,21 +17,4 @@ fn run_cmds() {
         let stdout = String::from_utf8(output.stdout).unwrap();
         println!("The stdout of binary {} is: {}", i + 1, stdout);
     }
-}
-
-fn main() {
-    let args = Args::parse();
-
-    /// Read the config
-    let f = std::fs::File::open(args.config).expect("Could not open file.");
-    let mut scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
-    println!("{:?}", scrape_config);
-
-    for wisker in scrape_config.wiskers.iter() {
-        println!(
-            "name: {}, binary: {}, args {}",
-            wisker.name, wisker.binary, wisker.args
-        );
-    }
-
 }
