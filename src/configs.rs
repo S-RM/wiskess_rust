a95904da70c32f34e42ffc69cd8bdc20ede9c464
vividDuck <122105925+vividDuck@users.noreply.github.com>
2023-08-18 09:59:45 +0000

    prompt for existing file

---
+++ src/configs.rs
@@ -0,0 +1,37 @@
+pub mod config {
+  use serde::{Deserialize, Serialize};
+
+  /// Top level structure of config file
+  #[derive(Debug, Serialize, Deserialize)]
+  pub struct Config {
+      pub artefacts: Vec<Artefacts>,
+      pub wiskers: Vec<Wiskers>,
+  }
+
+  /// Configuration of the commands to run
+  #[derive(Debug, Serialize, Deserialize)]
+  pub struct Wiskers {
+    pub name: String,
+    pub binary: String,
+    pub args: String,
+    pub outfolder: String,
+    #[serde(default)]
+    pub outfile: String,
+    #[serde(default)]
+    pub choco: String,
+    #[serde(default)]
+    pub github: String,
+    #[serde(default)]
+    pub deps_choco: String,
+    #[serde(default)]
+    pub deps_github: String,
+  }
+
+  /// Artefact paths and type
+  #[derive(Debug, Serialize, Deserialize)]
+  pub struct Artefacts {
+    pub name: String,
+    pub path: String,
+    pub path_type: String,
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,60 @@
-use clap::Parser;
-use serde::{Deserialize, Serialize};
+mod configs;
+
+use crate::configs::config;
 use serde_yaml::{self};
 use std::process::{Command, Output};
 use std::time::Duration;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::thread;
+use inquire::Confirm;
+use std::fs;
+use clap::Parser;
+use std::path::Path;
 
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Config {
-    wiskers: Vec<Wiskers>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Wiskers {
-  name: String,
-  binary: String,
-  args: String,
-  outfolder: String,
-  #[serde(default)]
-  outfile: String,
-  #[serde(default)]
-  choco: String,
-  #[serde(default)]
-  github: String,
-  #[serde(default)]
-  deps_choco: String,
-  #[serde(default)]
-  deps_github: String,
-}
-
-/// Simple program to greet a person
+/// Structure of the command line args
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Config file
     #[arg(short, long)]
     config: String,
+
+    /// Output file
+    #[arg(short, long)]
+    logfile: String,
+}
+
+fn file_exists(file_path: &String) {
+    println!("[+] Opening file: {file_path}");
+    // match fs::metadata(file_path) {
+    //     Ok(_) => exists = true,
+    //     Err(_) => exists = false,
+    // }
+
+    let path = Path::new(&file_path);
+    if path.exists() {
+        let ans = Confirm::new("File exists. Do you want to overwrite the file?")
+            .with_default(false)
+            .with_help_message("Overwrite the file if you want to rerun the command.")
+            .prompt();
+
+        match ans {
+            Ok(true) => {
+                println!("That's awesome!");
+                let _ = OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(&file_path)
+                    .expect("Failed to overwrite file");
+            } 
+            Ok(false) => println!("Keeping original file."),
+            Err(_) => println!("No valid response to question."),
+        }
+    } else {
+        println!("File does not exist!");
+    }    
 }
 
 fn main() {
@@ -46,8 +63,12 @@ fn main() {
 
     // Read the config
     let f = std::fs::File::open(args.config).expect("Could not open file.");
-    let scrape_config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
+    let scrape_config: config::Config = serde_yaml::from_reader(f).expect("Could not read values.");
     println!("{:?}", scrape_config);
+
+    // Set log
+    let log_file = args.logfile;
+    file_exists(&log_file);
 
     // let mut commands = Vec::new();
     let mut children = vec![];
@@ -72,9 +93,9 @@ fn main() {
         .write(true)
         .create(true)
         .append(true)
-        .open("output.txt")
-        .expect("Failed to open 'output.txt'");
-
+        .open(&log_file)
+        .expect("Failed to open log file");
+        
     for child in children {
         let output = child.join().unwrap();
         file.write_all(&output).expect("Failed to write to 'output.txt'");
